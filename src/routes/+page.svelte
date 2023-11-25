30faedb8068de5e8269a707350b5591547e56395
Monofactori <monofactura@icloud.com>
2023-11-25 21:02:40 +0300

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,15 @@
 	<meta name="description" content="ravil yakupov" />
 </svelte:head>
 
-<div class="flex flex-col justify-start items-center h-screen mt-[30%]">
+<div class="flex flex-col justify-start items-center h-screen mt-[20%]">
 	<h1 class="text-center h1">Ravil Yakupov</h1>
 	<p class="text-center text-xl">furniture development and consultancy.</p>
 	<hr class="w-full max-w-xs my-4 border-t border-gray-300" />
     <div class="btn-group-vertical !bg-transparent btn-xl w-72">
-        <button class="bg-transparent py-2 my-1">contact</button>
-        <button class="bg-transparent py-2 my-1">experience</button>
-        <button class="bg-transparent py-2 my-1">projects</button>
-        <button class="bg-transparent py-2 my-1">about me</button>
+        <a href="/contact" class="bg-transparent py-2 my-1">contact</a>
+        <a href="/experience" class="bg-transparent py-2 my-1">experience</a>
+        <a href="/projects" class="bg-transparent py-2 my-1">projects</a>
+        <a href="/about-me" class="bg-transparent py-2 my-1">about me</a>
     </div>
 </div>
 
