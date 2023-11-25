a6d87ddcd1b38cfaaaf89416815507676f6f4818
Monofactori <monofactura@icloud.com>
2023-11-25 10:23:11 +0300

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,25 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="ravil yakupov" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div class="flex flex-col justify-start items-center h-screen mt-[30%]">
+	<h1 class="text-center h1">Ravil Yakupov</h1>
+	<p class="text-center text-xl">furniture development and consultancy.</p>
+	<hr class="w-full max-w-xs my-4 border-t border-gray-300" />
+	<div class="btn-group-vertical variant-ghost btn-xl">
+		<button>contact</button>
+		<button>portfolio</button>
+		<button>projects</button>
+		<button>about me</button>
+	</div>
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
